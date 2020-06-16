a89e259b806a280553976da093af74b3d5d6ea06
한수현 <soohyun9903@naver.com>
2020-06-16 16:54:47 +0900

    수현 0616 16:00       파일  옮김       수현 0616 16:54       R.layout 수정

--- app/src/main/java/com/example/connector/soohyun/Login.java
+++ app/src/main/java/com/example/connector/soohyun/Login.java
@@ -8,10 +8,11 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.connector.R;
+import com.example.connector.activity.MainPageActivity;
 
 public class Login extends AppCompatActivity {
 
-    Button fjoin, fid;
+    Button fjoin, fid, loginBtn;
 
 
     @Override
@@ -20,6 +21,15 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.login);
 
         Intent intent = getIntent();
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainPageActivity.class);
+                startActivity(intent);
+            }
+        });
 
         fid.setOnClickListener(new View.OnClickListener() {
             @Override
