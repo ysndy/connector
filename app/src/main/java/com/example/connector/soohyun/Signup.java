78224c8e10b329f534606bfd87110e84fc6e9f54
한수현 <soohyun9903@naver.com>
2020-06-18 23:07:04 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/drawable-v24/nullItem.PNG
    #	app/src/main/res/drawable-v24/nullPro.PNG
    #	app/src/main/res/drawable-v24/null_item.PNG
    #	app/src/main/res/drawable-v24/null_pro.PNG
    #	app/src/main/res/drawable-v24/nullitem.PNG
    #	app/src/main/res/drawable-v24/nullpro.PNG
    #	app/src/main/res/drawable-v24/proImg.PNG
    #	app/src/main/res/drawable-v24/pro_img.PNG
    #	app/src/main/res/drawable-v24/proimg.PNG
    #	app/src/main/res/layout/login.xml
    #	app/src/main/res/layout/res_editrequest.xml
    #	app/src/main/res/layout/rest_editpage.xml
    #	app/src/main/res/layout/rest_mypage.xml
    #	app/src/main/res/layout/rest_nowrequest.xml
    #	app/src/main/res/layout/rest_profile.xml

--- app/src/main/java/com/example/connector/soohyun/Signup.java
+++ app/src/main/java/com/example/connector/soohyun/Signup.java
@@ -1,5 +1,6 @@
 package com.example.connector.soohyun;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,7 @@ public class Signup extends AppCompatActivity {
 
     EditText joinId, jpinPass, joinEmail, joinNum;
     TextView joinAgree;
-    Button goBtn;
+    Button goBtn, emailver;
     CheckBox checkAgree;
 
 
@@ -33,6 +34,7 @@ public class Signup extends AppCompatActivity {
         joinNum = findViewById(R.id.joinNum);
         joinAgree = findViewById(R.id.goBtn);
         checkAgree = findViewById(R.id.checkAgree);
+        emailver = findViewById(R.id.email_ver);
 
         Intent intent = getIntent();
 
@@ -41,12 +43,29 @@ public class Signup extends AppCompatActivity {
             public void onClick(View v) {
                 AlertDialog.Builder dlg = new AlertDialog.Builder(Signup.this);
                 dlg.setTitle("회원가입 완료");
-                dlg.setMessage("축하합니다. \n Go래처 회원이 되었습니다.");
-                dlg.setPositiveButton("확인", null);
+                dlg.setMessage("축하합니다.\nGo래처 회원이 되었습니다.\n로그인해주세요. ");
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+
                 dlg.show();
             }
         });
 
+        emailver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(Signup.this);
+                dlg.setTitle("이메일 인증");
+                dlg.setMessage("메일함을 확인하여 인증해주세요.");
+                dlg.setPositiveButton("확인",null);
+            }
+        });
     }
 
 }
