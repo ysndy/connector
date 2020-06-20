249fcc2fbf08074d11770023abd18b141f92d779
한수현 <soohyun9903@naver.com>
2020-06-20 14:41:25 +0900

    수현 탭뷰수정중

--- app/src/main/java/com/example/connector/soohyun/tabs/NowTab_request_Adapter.java
+++ app/src/main/java/com/example/connector/soohyun/tabs/NowTab_request_Adapter.java
@@ -1,4 +1,52 @@
 package com.example.connector.soohyun.tabs;
 
-public class NowTab_request_Adapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.example.connector.R;
+import com.example.connector.doyeon.objects.Transaction;
+
+import java.util.ArrayList;
+
+public class NowTab_request_Adapter extends BaseAdapter {
+
+    private ArrayList<Transaction> list;
+
+    public NowTab_request_Adapter(ArrayList<Transaction> list) {
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        NowTab_ing_Adapter.CustomViewHolder holder;
+
+        if (convertView == null){
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.restitem_fragnow1,null,false);
+
+
+        }
+
+        return convertView;
+    }
+
+    public class CustomViewHolder  {
+    }
 }
