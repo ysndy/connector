27ae9a1e64b0924bf0039e5e931c43d729298b42
한수현 <soohyun9903@naver.com>
2020-06-21 15:28:29 +0900

    수현 수정중

--- app/src/main/java/com/example/connector/soohyun/tabs/NowTab_ing_Adapter.java
+++ app/src/main/java/com/example/connector/soohyun/tabs/NowTab_ing_Adapter.java
@@ -1,5 +1,6 @@
 package com.example.connector.soohyun.tabs;
 
+import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import com.example.connector.R;
 import com.example.connector.doyeon.objects.Transaction;
+import com.example.connector.soohyun.restaurantpage.NowRequest;
 import com.example.connector.soohyun.shared.Login;
 
 import java.util.ArrayList;
@@ -18,6 +20,7 @@ import java.util.ArrayList;
 public class NowTab_ing_Adapter extends BaseAdapter {
 
     private ArrayList<Transaction> list;
+    private Context context;
 
     public NowTab_ing_Adapter(ArrayList<Transaction> list) {
         this.list = list;
@@ -42,6 +45,7 @@ public class NowTab_ing_Adapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         NowTab_ing_Adapter.CustomViewHolder holder;
 
+        this.context = parent.getContext();
         if (convertView == null){
             convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.restitem_fragnow2,null,false);
 
@@ -62,15 +66,15 @@ public class NowTab_ing_Adapter extends BaseAdapter {
         holder.now2ItemSupply.setText(transaction.getSupplyName());
         holder.now2ItemReqDate.setText(transaction.getDate());
 
-        /*
+
         holder.yellBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Login.class);
-                startActivity(intent);
+                Intent intent = new Intent(context, NowRequest.class);
+                context.startActivity(intent);
             }
         });
-*/
+
         return convertView;
     }
 
