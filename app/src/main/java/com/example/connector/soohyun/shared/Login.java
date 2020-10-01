2e81fb0696ab1abdf415cf459292f98f2f51d94e
한수현 <soohyun9903@naver.com>
2020-10-02 01:01:38 +0900

    Google API설정

--- app/src/main/java/com/example/connector/soohyun/shared/Login.java
+++ app/src/main/java/com/example/connector/soohyun/shared/Login.java
@@ -2,6 +2,7 @@ package com.example.connector.soohyun.shared;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,18 +17,35 @@ import com.example.connector.soohyun.restaurantpage.EditRequest;
 import com.example.connector.soohyun.restaurantpage.MyPage;
 import com.example.connector.soohyun.restaurantpage.NowRequest;
 import com.example.connector.soohyun.restaurantpage.RestaurantProfileActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class Login extends AppCompatActivity {
 
+    public static final int RC_SIGN_IN = 1; //구글 API
+    private GoogleSignInClient mGoogleSignInClient; //구글 API
 
     ImageView login_logoImg;
     EditText ID, Password;
-    Button login1Btn, login2Btn, fjoin, fid;
+    Button login1Btn, login2Btn, fjoin, fid, shareLoginBtn;
+    SignInButton googleBtn;//구글 로그인 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        /*구글API*/
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso); //구글API
 
         login_logoImg = findViewById(R.id.login_logoImg);
         ID = findViewById(R.id.ID);
@@ -36,16 +54,32 @@ public class Login extends AppCompatActivity {
         login2Btn = findViewById(R.id.login2Btn);
         fjoin = findViewById(R.id.fjoin);
         fid = findViewById(R.id.fid);
+        shareLoginBtn = findViewById(R.id.ShareLoginBtn);
+        googleBtn = findViewById(R.id.googleBtn);
 
         Intent intent = getIntent();
 
-        fjoin.setOnClickListener(new View.OnClickListener() {
+        /*구글API*/
+        googleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Signup.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.googleBtn:
+                        Log.d("@@@@","@@@@");
+                    signIn();
+                    break;
+                }
             }
         });
+
+       fjoin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(getApplicationContext(), Signup.class);
+               startActivity(intent);
+           }
+       });
+
 
         fid.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,4 +106,46 @@ public class Login extends AppCompatActivity {
         });
     }
 
+    /*getSignInIntent 메서드로 로그인 의도 만들고 의도 사용하여 로그인 버튼 탭 처리*/
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    /*사용자가 로그인한 후 활동 방법에서 사용자에 대한 GoogleSignInAccount개체 얻음*/
+    /*개체에는 사용자의 이름과 같이 로그인한 사용자 저보 포함되어 있음. GoogleSignInAccount*/
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    /*getEmail, getId를사용하여 사용자의 Google ID(클라이언트 측 사용용)와 getIdToken을 */
+    /*사용하는 사용자의 이메일 주소와 getIdToken을사용하는 사용자를 위한 ID 토큰을 얻을 수도 있음*/
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            String googleEmail = account.getEmail(); //로그인한 사용자의 이메일 주소 반환
+            String googleId = account.getId(); //Google 계정에 대한 고유 ID 반환
+            String googleName =  account.getDisplayName(); //서명된 사용자의 표시 이름 반환
+            Log.d("Email", googleEmail);
+            Log.d("Id", googleId);
+            Log.d("Name", googleName);
+            /*// Signed in successfully, show authenticated UI.
+            updateUI(account);*/
+        } catch (ApiException e) {
+           /* // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);*/
+        }
+    }
 }
