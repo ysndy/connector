f27ea44e541e64f01c7f92b5d1ed4bbb290315a8
한수현 <soohyun9903@naver.com>
2020-10-04 15:39:49 +0900

    Google API설정

--- app/src/main/java/com/example/connector/soohyun/shared/Login.java
+++ app/src/main/java/com/example/connector/soohyun/shared/Login.java
@@ -1,16 +1,24 @@
 package com.example.connector.soohyun.shared;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.connector.MainActivity;
 import com.example.connector.R;
 import com.example.connector.doyeon.activity.MainPageActivity;
 import com.example.connector.soohyun.restaurantpage.EditRequest;
@@ -24,6 +32,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.kakao.sdk.auth.LoginClient;
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Login extends AppCompatActivity {
 
@@ -32,13 +46,23 @@ public class Login extends AppCompatActivity {
 
     ImageView login_logoImg;
     EditText ID, Password;
-    Button login1Btn, login2Btn, fjoin, fid, shareLoginBtn;
+    Button login1Btn, naverLogin, fjoin, fid, shareLoginBtn;
     SignInButton googleBtn;//구글 로그인 버튼
+
+    OAuthLogin mOAuthLoginModule;//네이버
+    Context mContext; //네이버
+
+   //네이버Client
+    private static String OAUTH_CLIENT_ID = "uMzH56lm9EaQQmvV4jJm" ;
+    private static String OAUTH_CLIENT_SECRET = "c0mTBFEUDa" ;
+    private static String OAUTH_CLIENT_NAME = "고래처";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        getHashKey(); //해시값가져오기
 
         /*구글API*/
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -51,7 +75,7 @@ public class Login extends AppCompatActivity {
         ID = findViewById(R.id.ID);
         Password = findViewById(R.id.Password);
         login1Btn = findViewById(R.id.login1Btn);
-        login2Btn = findViewById(R.id.login2Btn);
+        naverLogin = findViewById(R.id.naverLogin);
         fjoin = findViewById(R.id.fjoin);
         fid = findViewById(R.id.fid);
         shareLoginBtn = findViewById(R.id.ShareLoginBtn);
@@ -63,28 +87,89 @@ public class Login extends AppCompatActivity {
         googleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switch (view.getId()){
+                switch (view.getId()) {
                     case R.id.googleBtn:
-                        Log.d("@@@@","@@@@");
-                    signIn();
-                    break;
+                        Log.d("@@@@", "@@@@");
+                        signIn();
+                        break;
                 }
             }
         });
 
-       fjoin.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Intent intent = new Intent(getApplicationContext(), Signup.class);
-               startActivity(intent);
-           }
-       });
+        /*네이버로그인*/
+        naverLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mOAuthLoginModule = OAuthLogin.getInstance();
+                /*mOAuthLoginModule.init(
+                        mContext
+                        *//*, getString(R.string.naver_client_id)
+                        , getString(R.string.naver_client_secret)
+                        , getString(R.string.naver_client_name)*//*
+                        //, OAUTH_CALLBACK_INTENT
+                        // SDK 4.1.4 버전부터는 OAUTH_CALLBACK_INTENT변수를 사용하지 않습니다.
+                );*/
+                mOAuthLoginModule.init(
+                        mContext
+                        ,OAUTH_CLIENT_ID
+                        ,OAUTH_CLIENT_SECRET
+                        ,OAUTH_CLIENT_NAME
+                );
+
+                @SuppressLint("HandlerLeak")
+                OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+                    @Override
+                    public void run(boolean success) {
+                        if (success) {
+                            String accessToken = mOAuthLoginModule.getAccessToken(mContext);
+                            String refreshToken = mOAuthLoginModule.getRefreshToken(mContext);
+                            long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
+                            String tokenType = mOAuthLoginModule.getTokenType(mContext);
+
+                            Log.i("LoginData", "accessToken : " + accessToken);
+                            Log.i("LoginData", "refreshToken : " + refreshToken);
+                            Log.i("LoginData", "expiresAt : " + expiresAt);
+                            Log.i("LoginData", "tokenType : " + tokenType);
+
+                        } else {
+                            String errorCode = mOAuthLoginModule
+                                    .getLastErrorCode(mContext).getCode();
+                            String errorDesc = mOAuthLoginModule.getLastErrorDesc(mContext);
+                            Toast.makeText(mContext, "errorCode:" + errorCode
+                                    + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                    ;
+                };
+
+                mOAuthLoginModule.startOauthLoginActivity(Login.this, mOAuthLoginHandler);
+            }
+
+        });
+
+        fjoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+            }
+        });
 
 
         fid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Find_ID_Pass.class);
+                startActivity(intent);
+            }
+        });
+
+        shareLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainPageActivity.class);
                 startActivity(intent);
             }
         });
@@ -97,13 +182,6 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        login2Btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MyPage.class);
-                startActivity(intent);
-            }
-        });
     }
 
     /*getSignInIntent 메서드로 로그인 의도 만들고 의도 사용하여 로그인 버튼 탭 처리*/
@@ -148,4 +226,27 @@ public class Login extends AppCompatActivity {
             updateUI(null);*/
         }
     }
+
+    /*해시값가져오기위한 코드*/
+    private void getHashKey() {
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
+
 }
